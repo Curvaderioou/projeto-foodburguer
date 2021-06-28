909d2f8119642b8c8928fe16c19599dc83795e39
Curvaderioou <felipedegenerportella@yahoo.com>
2021-06-28 16:23:41 -0300

    projeto site para lanchonete, primeiros projetos realizados 2020

---
+++ js/script.js
@@ -0,0 +1,9 @@
+new SimpleSlide({
+  slide: 'slidemenu', // nome do atributo data-slide="principal"
+  auto: true, // se o slide deve passar automaticamente
+  nav: true, // se deve ou não mostrar a navegação
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+});
+
+new SimpleAnime();
